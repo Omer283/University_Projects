44b5356e4eb55b74b636f7b52bcbb8a62866fb43
Omer283 <54413696+Omer283@users.noreply.github.com>
2023-08-05 19:18:24 +0300

    Delete Matlab Project directory

--- Matlab Project/ExB.m
+++
@@ -1,7 +0,0 @@
-boardSize=input('Board Size: ');
-periBegins = searchPeriods(boardSize);
-for i=1:length(periBegins)
-    x=periBegins(i);
-    numToMat(x,boardSize)
-end
-amount= length(periBegins)
--- Matlab Project/ExC.m
+++
@@ -1,7 +0,0 @@
-boardSize=[5 4]
-periBegins = searchPeriods(boardSize);
-for i=1:length(periBegins)
-    x=periBegins(i);
-    numToMat(x,boardSize)
-end
-amount= length(periBegins)
